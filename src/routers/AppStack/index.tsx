890b6968fc9a904d409d9b49e0faf1423aad2a77
mei-1405 <smeiyammai1@email.com>
2024-01-18 11:25:46 +0530

    first basic skeleton

--- src/routers/AppStack/index.tsx
+++ src/routers/AppStack/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createNativeStackNavigator } from `'@react-navigation/native-stack`';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { Welcome } from '../../screens';
 import { Login } from '../../screens';
